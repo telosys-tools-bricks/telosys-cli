132ea8a4a7d3aad774aa981a61253671d2e3afbe
Laurent Guerin <laurent.guerin@sogeti.com>
2017-09-22 10:54:01 +0200

    License header file + plugin in pom.xml + update all java files

--- src/main/java/org/telosys/tools/cli/commons/GitHubBundlesUtil.java
+++ src/main/java/org/telosys/tools/cli/commons/GitHubBundlesUtil.java
@@ -1,3 +1,18 @@
+/**
+ *  Copyright (C) 2015-2017  Telosys project org. ( http://www.telosys.org/ )
+ *
+ *  Licensed under the GNU LESSER GENERAL PUBLIC LICENSE, Version 3.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *          http://www.gnu.org/licenses/lgpl.html
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 package org.telosys.tools.cli.commons;
 
 import java.util.List;
