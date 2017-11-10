6b1907505ee644a9d4267b572b7beccf00cc3167
l-gu <laurent.guer1@gmail.com>
2017-11-10 11:11:39 +0100

    Version "CLI-006"

--- src/main/java/org/telosys/tools/cli/Version.java
+++ src/main/java/org/telosys/tools/cli/Version.java
@@ -19,6 +19,6 @@ import org.telosys.tools.generator.GeneratorVersion;
 
 public class Version {
 
-	public final static String TELOSYS_TOOLS_CLI_VERSION = GeneratorVersion.GENERATOR_VERSION + " CLI-005" ;
+	public final static String TELOSYS_TOOLS_CLI_VERSION = GeneratorVersion.GENERATOR_VERSION + " CLI-006" ;
 	
 }
