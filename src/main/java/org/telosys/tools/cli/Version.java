2d617ae6d3bba45ae16591e91b4c6c02f442f0a6
l-gu <laurent@telosys.org>
2021-08-19 19:14:19 +0200

    CLI version 3.4.0 (using Telosys-Core 3.4.0)

--- src/main/java/org/telosys/tools/cli/Version.java
+++ src/main/java/org/telosys/tools/cli/Version.java
@@ -20,7 +20,7 @@ public class Version {
 	/**
 	 * Current version (printed in banner and 'ver' command)
 	 */
-	public static final String TELOSYS_TOOLS_CLI_VERSION = "3.3.0 CLI-003" ;
+	public static final String TELOSYS_TOOLS_CLI_VERSION = "3.4.0 CLI-001" ;
 	
 	/**
 	 * Private constructor
